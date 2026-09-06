c95e0345a16540c32870a35e5811dcf0dd47a761
Musfira Jamil <musfirajamil@gmail.com>
2026-09-06 21:22:15 +1000

    Restore posts via modules + 19 primer posts

--- content/posts.ts
+++ content/posts.ts
@@ -1,1 +1,26 @@
-FILE_CONTENT_FROM:/workspace/primer-posts/posts.merged.ts
+import { existingPostsA } from './posts-existing-a'
+import { existingPostsB } from './posts-existing-b'
+import { primerPosts0 } from './posts-primers-0'
+import { primerPosts1 } from './posts-primers-1'
+import { primerPosts2 } from './posts-primers-2'
+import { primerPosts3 } from './posts-primers-3'
+import type { Post, PostCategory } from './post-types'
+
+export type { Post, PostCategory } from './post-types'
+
+export const posts: Post[] = [
+  ...primerPosts0,
+  ...primerPosts1,
+  ...primerPosts2,
+  ...primerPosts3,
+  ...existingPostsA,
+  ...existingPostsB,
+]
+
+export function getPost(slug: string) {
+  return posts.find((p) => p.slug === slug)
+}
+
+export function getPostsByCategory(category: PostCategory) {
+  return posts.filter((p) => p.category === category)
+}
